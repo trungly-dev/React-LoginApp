3a127e661a331279407e2e5ad1cc560ed1ef99b4
TrungLy <53240269+trungly-dev@users.noreply.github.com>
2020-12-14 14:41:49 +1100

    Add files via upload

---
+++ backend/Main.js
@@ -0,0 +1,54 @@
+const express       = require('express');
+const app           = express();
+const path          = require('path');
+const mysql         = require('mysql');
+const session       = require('express-session');
+const MySQLStore    = require('express-mysql-session')(session);
+const Router        = require('./Router');
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.json());
+
+console.log('Testing server');
+// Database
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306, 
+    user: 'root',
+    password: 'thisispasswork',
+    database: 'reactLogin' 
+});
+
+db.connect(function(err) {
+    if (err) {
+        console.log('DB error');
+        throw err;
+        return false;
+    }
+});
+
+const sessionStore = new MySQLStore({
+    expiration: (1825 * 86400 * 1000),
+    endConnectionOnClose: false
+} , db );
+
+app.use(session({
+    key: 'keyRandomKey',
+    secret: 'secretRandomSecret',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: (1825 * 86400 * 1000),
+        httpOnly: false
+    }
+}));
+
+new Router(app,db);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(3000);
